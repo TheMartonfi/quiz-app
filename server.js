60b74c20d26d5c6e8a9f5b9f1f80ca1b271479c0
TheMartonfi <carlmartonfi@hotmail.com>
2020-09-19 19:13:34 +0000

    Make header partial

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/quiz", quizRoutes(db));
 
 app.get("/", (req, res) => {
   // Pass in db info as templatevars
-  res.render("home");
+  res.render("take-quiz");
 });
 
 app.listen(PORT, () => {
