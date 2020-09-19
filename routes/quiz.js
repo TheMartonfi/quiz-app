32b7768d7ef1cfca0dfb00b02dbfb442e6ff5ecd
ninjabattler <jaydenrtucker@gmail.com>
2020-09-19 19:53:02 +0000

    Added the templateVars for the /quiz/:id route

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const queries = require('../db/queries');
 
 module.exports = (db) => {
   router.get("/:id", (req, res) => {
     // Render ejs file
       // Pass in db info via queries
+    queries.getQuiz(db, {id: req.params.id})
+    .then((quiz) => {
+      queries.getQuestions(db, {id: req.params.id})
+      .then((questions) => {
+        
+        res.render("index", {quiz, questions});
+      });
+    });
   });
 
   router.get("/:id/result/:id", (req, res) => {
