f8c8ed35c200522e102654b0860ce52bda543d01
ninjabattler <jaydenrtucker@gmail.com>
2020-09-21 22:23:48 +0000

    The take-quiz page now inserts a new result to the database

--- db/queries.js
+++ db/queries.js
@@ -115,10 +115,10 @@ exports.getQuestions = getQuestions;
 //Inserts and returns one new question object
 const insertNewResult = function(db, options){
   return db.query(`
-  INSERT INTO results (quiz_id, user_id, result, time_spent, quiz_rating)
-  VALUES ($1, $2, $3, $4, $5)
+  INSERT INTO results (quiz_id, user_id, result, quiz_rating)
+  VALUES ($1, $2, $3, $4)
   RETURNING *`,
-  [options.quiz_id, options.user_id, options.result, options.time_spent, options.quiz_rating,])
+  [options.quiz_id, options.user_id, options.result, options.quiz_rating,])
   .then(res => res.rows[0]);
 }
 exports.insertNewResult = insertNewResult;
