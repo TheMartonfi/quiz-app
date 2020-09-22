1024d394eb54eaa18d32bef4515175de45dc69e5
ninjabattler <jaydenrtucker@gmail.com>
2020-09-22 17:04:13 +0000

    Folder for client side scripts

---
+++ views/scripts/results.js
@@ -0,0 +1,22 @@
+function copy(){
+  const copyText = document.getElementById("link-botw");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the link: " + copyText.value);
+  copyText.remove();
+}
+
+$(function() {
+  $(".share-result").on('click', function(event){
+    event.preventDefault();
+    $('body').append(`<input id="link-botw" value='localhost:8080/quiz/${$(this).attr('usId')}/result/${$(this).attr('quId')}'>`);
+    copy();
+    
+  });
+  $(".share-quiz").on('click', function(event){
+    event.preventDefault();
+    $('body').append(`<input id="link-botw" value='localhost:8080/quiz/${$(this).attr('quId')}'>`);
+    copy();
+  });
+})
