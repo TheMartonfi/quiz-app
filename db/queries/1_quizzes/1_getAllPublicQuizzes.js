923a3f9ac82704b38e557e5a2de9c3bae7bf233a
TheMartonfi <carlmartonfi@hotmail.com>
2020-09-23 21:05:26 +0000

    Add time limit to home screen

--- db/queries/1_quizzes/1_getAllPublicQuizzes.js
+++ db/queries/1_quizzes/1_getAllPublicQuizzes.js
@@ -1,7 +1,7 @@
 //Returns an array of quizzes
 const getAllPublicQuizzes = function(db){
   return db.query(`
-  SELECT quizzes.id, title, category, description, COUNT(results.*) as times_played, AVG(quiz_rating) as average_rating, quizzes.image
+  SELECT quizzes.id, title, category, description, time_limit, COUNT(results.*) as times_played, AVG(quiz_rating) as average_rating, quizzes.image
   FROM quizzes
   FULL OUTER JOIN results ON quizzes.id = results.quiz_id
   WHERE NOT is_unlisted = true
