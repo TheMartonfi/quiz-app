bf372875de4234546635627059faab876b4d2527
ninjabattler <jaydenrtucker@gmail.com>
2020-09-23 17:45:31 +0000

    Users can now submit images for quizzes

--- db/queries/2_questions/2_getQuestions.js
+++ db/queries/2_questions/2_getQuestions.js
@@ -1,7 +1,7 @@
 //Returns a quiz object
 const getQuestions = function(db, options){
   return db.query(`
-  SELECT question, answer_1, answer_2, answer_3, answer_correct
+  SELECT question, answer_1, answer_2, answer_3, answer_correct, image
   FROM questions_and_answers
   JOIN quizzes ON questions_and_answers.quiz_id = quizzes.id
   WHERE quizzes.id = ${options.id};`)
