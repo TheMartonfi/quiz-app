5c8a4f8f722be15aa8d4ab37e7f79339137d8f0b
ninjabattler <jaydenrtucker@gmail.com>
2020-09-23 18:47:40 +0000

    Added the option for image banners on quizzes

--- db/queries/1_quizzes/6_searchQuizzes.js
+++ db/queries/1_quizzes/6_searchQuizzes.js
@@ -1,7 +1,7 @@
 //Returns an array of quizzes
 const searchQuizzes = function(db, options){
   return db.query(`
-  SELECT quizzes.id, title, category, description, COUNT(results.*) as times_played, AVG(quiz_rating) as average_rating
+  SELECT quizzes.id, title, category, description, COUNT(results.*) as times_played, AVG(quiz_rating) as average_rating, image
   FROM quizzes
   FULL OUTER JOIN results ON quizzes.id = results.quiz_id
   WHERE NOT is_unlisted = true
