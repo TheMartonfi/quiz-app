5b5c6df3a1885d4defacace14686f57b4ef2bb38
ninjabattler <jaydenrtucker@gmail.com>
2020-09-24 19:53:33 +0000

    Added more information to the delete quizzes page

--- db/queries/1_quizzes/3_getAllUsersQuizzes.js
+++ db/queries/1_quizzes/3_getAllUsersQuizzes.js
@@ -1,8 +1,9 @@
 const getAllUsersQuizzes = function(db, options){
   return db.query(`
-  SELECT quizzes.id, title, category, description, image, users.username as username
+  SELECT quizzes.id, title, category, description, image, users.username as username,COUNT(results.*) as times_played, AVG(quiz_rating) as average_rating, time_limit
   FROM quizzes
   JOIN users ON quizzes.owner_id = users.id
+  JOIN results ON quizzes.id = results.quiz_id
   WHERE owner_id = $1
   GROUP BY quizzes.id, title, category, description, users.username;`, [options.id])
   .then(res => res.rows);
